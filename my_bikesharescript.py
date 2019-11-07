ed5129018560baed0fa8d6cf8aa28c16bc1c3918
Kumar <V-Arun.Kumar@shell.com>
2019-11-07 23:25:57 +0530

    line 140 changed with detailed line comment

--- my_bikesharescript.py
+++ my_bikesharescript.py
@@ -137,7 +137,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays statistics on the total and average trip duration in the most granular level of seconds."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
